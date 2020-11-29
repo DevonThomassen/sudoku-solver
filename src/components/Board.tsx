26bbb8678815ab838a83bafceb1284f0e0bb5974
Devon Thomassen <devon.thomassen@hotmail.com>
2020-11-29 02:11:40 +0100

    Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import Box from './Box';
+import CellData from '../interfaces/CellData';
+import Sudoku from '../classes/sudoku';
 
-const Board = () => {
-  return (
-    <React.Fragment>
-      <h1>Sudoku solver</h1>
+interface Props {
+  sudoku: Sudoku;
+}
+
+interface State {
+  boxes: number[][];
+}
+
+class Board extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      boxes: this.loadBoxes()
+    }
+
+  }
+
+  private loadBoxes = (): number[][] => {
+    const sudoku = this.props.sudoku;
+    let _boxes: number[][] = [];
+    for (let i = 0; i < 9; i++) {
+      _boxes.push(sudoku.getBox(i));
+    }
+    return _boxes;
+  }
+
+  render() {
+    return (
       <div className="board">
-        {[...Array(9)].map((e, i) =>
+        {this.state.boxes.map((e, i) =>
           <Box
             key={i}
+            data={e}
             value={i}
+            update={(cellData: CellData) => this.props.sudoku.updateGrid(cellData)}
           />)}
       </div>
-    </React.Fragment>
-  );
+    );
+  }
 }
 
 export default Board;
