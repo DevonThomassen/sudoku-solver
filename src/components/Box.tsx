bcf641cd1dc8ac933cee7fa01782544269c1d507
Devon Thomassen <devon.thomassen@hotmail.com>
2020-11-29 02:11:58 +0100

    Box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Cell from './Cell';
+import CellData from '../interfaces/CellData';
 
 interface Props {
+  data: number[];
   readonly value: number;
+  update: (cellData: CellData) => void;
 }
 
 const Box = (props: Props) => {
   return (
-    <div className={`box`} data-row={props.value}>
-      {[...Array(9)].map((e, i) =>
+    <div className={`box`}>
+      {props.data.map((e, i) =>
         <Cell
           key={i}
           row={props.value}
           col={i}
+          value={e}
+          update={(cellData: CellData) => props.update(cellData)}
         />)}
     </div>
   );
